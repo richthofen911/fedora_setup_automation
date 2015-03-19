81ddb1ec5bfbb58bb3f7f3e05ad7bfce79fab3ab
richthofen911 <yichaoli.richthofen@gmail.com>
2015-03-19 17:35:28 -0400

    Create fedora_setup_automation.sh

---
+++ fedora_setup_automation.sh
@@ -0,0 +1,38 @@
+#In settings, create alt+1, alt+2, alt+3 shortcuts 
+
+#sudo yum install google-chrome-stable -y
+#sudo yum install emacs -y
+#sudo yum remove java* -y
+#wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b26/jdk-8u40-linux-x64.rpm
+#sudo rpm -i Downloads/jdk-8u40-linux-x64.rpm -y
+#sudo yum install gnome-tweak-tool -y
+#sudo yum install dconf-editor -y
+#sudo yum remove ibus -y
+#sudo yum install fcitx -y
+#sudo yum install fcitx-libpinyin -y
+#sudo yum install fcitx-configtool -y
+#sudo yum install im-chooser -y
+#gsettings set org.gnome.settings-daemon.plugins.keyboard active false
+#sudo yum install httpd -y
+#sudo yum install nodejs -y
+#sudo yum install npm -y
+#sudo npm install express-generator -g
+#sudo yum install libgnome -y
+#sudo yum install php -y
+
+#touch Templates/newBlankDoc
+#touch Templates/newBlankPy.py
+#touch Templates/newBlankJS.js
+#echo '#!/bin/bash' > Templates/newBlankSh.shtouch Templates/newBlankSh.sh
+
+###------dropbox installation and setup
+#cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -
+#~/.dropbox-dist/dropboxd
+#echo "file:///home/richthofen80/Dropbox" >> ~/.config/gtk-3.0/bookmarks
+
+###------android development related
+#android studio latest download url, can be downloaded by crawler  http://tools.android.com/download/studio/canary/latest
+#android sdk download url, can be downloaded by crawler            http://developer.android.com/sdk/installing/index.html?pkg=tools
+
+#settings
+#gnome-tweak-tool(add window list and swap ctrl/capslock), im-chooser, fcitx-configtool, 

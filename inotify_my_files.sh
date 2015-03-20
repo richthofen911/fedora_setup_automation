407b21c4ef2bec01416bbb3b81d1d9cfac9d61d5
richthofen80 <richthofen80@localhost.localdomain>
2015-03-20 12:08:04 -0400

    auto-maintain

--- inotify_my_files.sh
+++ inotify_my_files.sh
@@ -4,7 +4,7 @@ file2=/home/richthofen80/.emacs
 
 dir1=/home/richthofen80/.emacs.d
 
-while inotifywait -rme modify,attrib,move,close_write $file1 $file2 $dir1; 
+while inotifywait -mre modify,attrib,move,close_write $file1 $file2 $dir1; 
 do
 	nohup /home/richthofen80/Programming/gitPro/fedora_setup_automation/sync_customized_settings_to_github.sh &
 done

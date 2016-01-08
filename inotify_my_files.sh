a8ae1ea7fe2ca8646fcfaf0c2f8b334420f52cfb
richthofen911 <yichaoli.richthofen@gmail.com>
2016-01-08 09:20:44 +0800

    update

--- inotify_my_files.sh
+++ inotify_my_files.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-file1=/home/richthofen80/.bashrc
-file2=/home/richthofen80/.emacs
 
-dir1=/home/richthofen80/.emacs.d
+# this file is not being used, it is just an example of how to use inotify
 
-while inotifywait -mre modify,attrib,move,close_write $file1 $file2 $dir1; 
-do
-	/home/richthofen80/Programming/fedora_setup_automation/sync_customized_settings_to_github.sh
-done
+#file1=/home/richthofen80/.bashrc
+#file2=/home/richthofen80/.emacs
+
+#dir1=/home/richthofen80/.emacs.d
+
+#while inotifywait -mre modify,attrib,move,close_write $file1 $file2 $dir1; 
+#do
+#	/home/richthofen80/Programming/fedora_setup_automation/sync_customized_settings_to_github.sh
+#done
 
